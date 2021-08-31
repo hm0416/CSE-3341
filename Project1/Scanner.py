91c4c068ec1257bd8f5e4bd76ddb35d8e40d6e3d
hm0416 <hm0416@Hifas-MBP-8.lan>
2021-08-30 22:14:45 -0400

    added code for current token

--- Project1/Scanner.py
+++ Project1/Scanner.py
@@ -11,8 +11,51 @@ class Scanner:
     return 0
 
   # currentToken should return the current token
-  def currentToken(self):
-    return 0
+  def currentToken(self, filename):
+    file = open(filename, "r")
+    currentChar = file.read()
+
+    if currentChar == ';':
+      return Core.SEMICOLON
+    elif currentChar == ',':
+      return Core.COMMA
+    elif currentChar == '!':
+      return Core.NEGATION
+    elif currentChar == '+':
+      return Core.ADD
+    elif currentChar == '-':
+      return Core.SUB
+    elif currentChar == '*':
+      return Core.MULT
+    elif currentChar == '(':
+      return Core.LPAREN
+    elif currentChar == ')':
+      return Core.RPAREN
+    elif currentChar == '=':
+      nextChar = self.nextToken()
+      if nextChar == '=':
+        #read char before return
+        return Core.EQUAL
+      else:
+        return Core.ASSIGN
+    elif currentChar == '<':
+      nextChar = self.nextToken()
+      if nextChar == '=':
+        #read char before return
+        return Core.LESSEQUAL
+      else:
+        return Core.LESS
+    elif currentChar.isalpha():
+      while(self.nextToken()):
+
+    elif currentChar.isnumeric():
+      while (self.nextToken()):
+
+
+
+
+
+
 
   # If the current token is ID, return the string value of the identifier
 	# Otherwise, return value does not matter
