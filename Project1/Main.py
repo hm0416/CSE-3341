0d77bfa4dbcaaca79bf0b38fca8455f41eb8f172
hm0416 <hm0416@hifas-mbp-8.attlocal.net>
2021-09-05 16:11:08 -0400

    got most of it working

--- Project1/Main.py
+++ Project1/Main.py
@@ -3,50 +3,123 @@ from Core import Core
 import sys
 
 def main():
-  # S = Scanner('/Users/hm0416/Desktop/CSE-3341/Project1/Correct/1.code')
-  #
-  # file = open('/Users/hm0416/Desktop/CSE-3341/Project1/Correct/1.code', "r")
-  # lines = file.readlines()
-  # tokens = []
-  # mergedTokenList = []
-  # for line in lines:
-  #   tokens.append(line.split())
-  #
-  # for tokenList in tokens:
-  #   mergedTokenList += tokenList
-  #
-  # for i, v in enumerate(mergedTokenList):
-  #   if ';' in v:
-  #     mergedTokenList[i] = v.replace(';', 'SEMICOLON')
-  #
-  # tok = S.nextToken('/Users/hm0416/Desktop/CSE-3341/Project1/Correct/1.code')
-  #
-  # print(mergedTokenList)
+    # S = Scanner('/Users/hm0416/Desktop/CSE-3341/Project1/Correct/1.code')
+    #
+    # file = open('/Users/hm0416/Desktop/CSE-3341/Project1/Correct/1.code', "r")
+    # lines = file.readlines()
+    # tokens = []
+    # mergedTokenList = []
+    # for line in lines:
+    #     tokens.append(line.split())
+    #
+    # for tokenList in tokens:
+    #     mergedTokenList += tokenList
+    # #
+    # for i, v in enumerate(mergedTokenList):
+    #     if ';' in v:
+    #         mergedTokenList[i] = v.replace(';', 'SEMICOLON')
+    #     elif ',' in v:
+    #         mergedTokenList[i] = v.replace(',', 'COMMA')
+    #     elif '!' in v:
+    #         mergedTokenList[i] = v.replace('!', 'NEGATION')
+    #     elif '+' in v:
+    #         mergedTokenList[i] = v.replace('+', 'ADD')
+    #     elif '-' in v:
+    #         mergedTokenList[i] = v.replace('-', 'SUB')
+    #     elif '*' in v:
+    #         mergedTokenList[i] = v.replace('*', 'MULT')
+    #     elif '(' in v:
+    #         mergedTokenList[i] = v.replace('(', 'LPAREN')
+    #     elif ')' in v:
+    #         mergedTokenList[i] = v.replace(')', 'RPAREN')
+    #     elif 'begin' in v:
+    #         mergedTokenList[i] = v.replace('begin', 'BEGIN')
+    #     elif 'program' in v:
+    #         mergedTokenList[i] = v.replace('program', 'PROGRAM')
+    #     elif 'end' in v:
+    #         mergedTokenList[i] = v.replace('end', 'END')
+    #     elif 'new' in v:
+    #         mergedTokenList[i] = v.replace('new', 'NEW')
+    #     elif 'define' in v:
+    #         mergedTokenList[i] = v.replace('define', 'DEFINE')
+    #     elif 'extends' in v:
+    #         mergedTokenList[i] = v.replace('extends', 'EXTENDS')
+    #     elif 'class' in v:
+    #         mergedTokenList[i] = v.replace('class', 'CLASS')
+    #     elif 'endclass' in v:
+    #         mergedTokenList[i] = v.replace('endclass', 'ENDCLASS')
+    #     elif 'int' in v:
+    #         mergedTokenList[i] = v.replace('int', 'INT')
+    #     elif 'endfunc' in v:
+    #         mergedTokenList[i] = v.replace('endfunc', 'ENDFUNC')
+    #     elif 'if' in v:
+    #         mergedTokenList[i] = v.replace('if', 'IF')
+    #     elif 'then' in v:
+    #         mergedTokenList[i] = v.replace('then', 'THEN')
+    #     elif 'else' in v:
+    #         mergedTokenList[i] = v.replace('else', 'ELSE')
+    #     elif 'while' in v:
+    #         mergedTokenList[i] = v.replace('while', 'WHILE')
+    #     elif 'endwhile' in v:
+    #         mergedTokenList[i] = v.replace('endwhile', 'ENDWHILE')
+    #     elif 'endif' in v:
+    #         mergedTokenList[i] = v.replace('endif', 'ENDIF')
+    #     elif 'input' in v:
+    #         mergedTokenList[i] = v.replace('input', 'INPUT')
+    #     elif 'output' in v:
+    #         mergedTokenList[i] = v.replace('output', 'OUTPUT')
+    #     elif 'ref' in v:
+    #         mergedTokenList[i] = v.replace('ref', 'REF')
+    #     elif 'eof' in v:
+    #         mergedTokenList[i] = v.replace('eof', 'EOF')
+    #     elif 'error' in v:
+    #         mergedTokenList[i] = v.replace('error', 'ERROR')
+    #     elif '==' in v:
+    #       mergedTokenList[i] = v.replace('==', 'EQUAL')
+    #     elif '=' in v:
+    #       mergedTokenList[i] = v.replace('=', 'ASSIGN')
+    #     elif '<' in v:
+    #       mergedTokenList[i] = v.replace('<', 'LESS')
+    #     elif '<=' in v:
+    #       mergedTokenList[i] = v.replace('<=', 'LESSEQUAL')
+    #     elif v.isalpha():
+    #       mergedTokenList[i] = v.replace(v, 'ID[' + v + "]")
+    #     elif v.isnumeric():
+    #       mergedTokenList[i] = v.replace(v, 'CONST[' + v + "]")
 
+    #
+    # tok = S.nextToken('/Users/hm0416/Desktop/CSE-3341/Project1/Correct/1.code')
+    #
+    # print(mergedTokenList)
 
+    # tempMergedTokenList = mergedTokenlist.copy()
 
+    # nextTok = tempMergedTokenList[0]
 
-  # tempMergedTokenList = mergedTokenlist.copy()
+    # Initialize the scanner with the input file
+    S = Scanner('/Users/hm0416/Desktop/CSE-3341/Project1/Correct/1.code')
+    intialTokensList = S.tokenizer('/Users/hm0416/Desktop/CSE-3341/Project1/Correct/1.code')
+    # firstTok = intialTokensList[0]
+    # S.nextToken('/Users/hm0416/Desktop/CSE-3341/Project1/Correct/1.code', intialTokensList)
 
-  # nextTok = tempMergedTokenList[0]
-
-  # Initialize the scanner with the input file
-  S = Scanner(sys.argv[1])
-
-  # Print the token stream
-  while (S.currentToken() != Core.EOF and S.currentToken() != Core.ERROR):
-    # Print the current token, with any extra data needed
-    print(S.currentToken().name, end='')
-    if (S.currentToken() == Core.ID):
-      value = S.getID()
-      print("[" + value + "]", end='')
-    elif (S.currentToken() == Core.CONST):
-      value = S.getCONST()
-      print("[" + str(value) + "]", end='')
-    print()
-    # Advance to the next token
-    S.nextToken()
+    # Print the token stream
+    while (S.currentToken(intialTokensList) != Core.EOF and S.currentToken(intialTokensList) != Core.ERROR):
+        # Print the current token, with any extra data needed
+        print(S.currentToken(intialTokensList).name, end='')
+        if (S.currentToken(intialTokensList) == Core.ID):
+            currentStr = intialTokensList[0]
+            value = S.getID(currentStr)
+            print("[" + value + "]", end='')
+        elif (S.currentToken(intialTokensList) == Core.CONST):
+            numStr = intialTokensList[0]
+            value = S.getCONST(numStr)
+            print("[" + str(value) + "]", end='')
+        print()
+        # Advance to the next token
+        intialTokensList = S.nextToken('/Users/hm0416/Desktop/CSE-3341/Project1/Correct/1.code', intialTokensList)
 
 
 if __name__ == "__main__":
     main()
+
+# sys.argv[1]
--- Project1/Scanner.py
+++ Project1/Scanner.py
@@ -5,7 +5,7 @@ class Scanner:
   # Constructor should open the file and find the first token
   def __init__(self, filename):
     file = open(filename, "r")
-    self.nextToken(filename)
+    # self.nextToken(filename, )
 
   #tokenzies everything, then we need to go through each element in the mergedTokenList and replace its value with what
   #it's supposed to be based on ENUM
@@ -14,7 +14,7 @@ class Scanner:
     lines = file.readlines()
     tokens = []
     mergedTokenList = []
-    global tempMergedTokenList
+    # global tempMergedTokenList
 
     for line in lines:
       tokens.append(line.split())
@@ -22,103 +22,111 @@ class Scanner:
     for tokenList in tokens:
       mergedTokenList += tokenList
 
-    tempMergedTokenList = mergedTokenList.copy()
-
-    # return mergedTokenlist[index + 1]
     # tempMergedTokenList = mergedTokenList.copy()
-    # return tempMergedTokenList.pop(0)
-
-    for i, v in enumerate(mergedTokenList):
-      if ';' in v:
-        mergedTokenList[i] = v.replace(';', 'SEMICOLON')
-      elif ',' in v:
-        mergedTokenList[i] = v.replace(',', 'COMMA')
-      elif '!' in v:
-        mergedTokenList[i] = v.replace('!', 'NEGATION')
-      elif '+' in v:
-        mergedTokenList[i] = v.replace('+', 'ADD')
-      elif  '-' in v:
-        mergedTokenList[i] = v.replace('-', 'SUB')
-      elif '*' in v:
-        mergedTokenList[i] = v.replace('*', 'MULT')
-      elif '(' in v:
-        mergedTokenList[i] = v.replace('(', 'LPAREN')
-      elif  ')' in v:
-        mergedTokenList[i] = v.replace(')', 'RPAREN')
-      elif 'begin' in v:
-        mergedTokenList[i] = v.replace('begin', 'BEGIN')
-      elif 'program' in v:
-        mergedTokenList[i] = v.replace('program', 'PROGRAM')
-      elif 'end' in v:
-        mergedTokenList[i] = v.replace('end', 'END')
-      elif 'new' in v:
-        mergedTokenList[i] = v.replace('new', 'NEW')
-      elif 'define' in v:
-        mergedTokenList[i] = v.replace('define', 'DEFINE')
-      elif 'extends' in v:
-        mergedTokenList[i] = v.replace('extends', 'EXTENDS')
-      elif 'class' in v:
-        mergedTokenList[i] = v.replace('class', 'CLASS')
-      elif 'endclass' in v:
-        mergedTokenList[i] = v.replace('endclass', 'ENDCLASS')
-      elif 'int' in v:
-        mergedTokenList[i] = v.replace('int', 'INT')
-      elif 'endfunc' in v:
-        mergedTokenList[i] = v.replace('endfunc', 'ENDFUNC')
-      elif 'if' in v:
-        mergedTokenList[i] = v.replace('if', 'IF')
-      elif 'then' in v:
-        mergedTokenList[i] = v.replace('then', 'THEN')
-      elif 'else' in v:
-        mergedTokenList[i] = v.replace('else', 'ELSE')
-      elif 'while' in v:
-        mergedTokenList[i] = v.replace('while', 'WHILE')
-      elif 'endwhile' in v:
-        mergedTokenList[i] = v.replace('endwhile', 'ENDWHILE')
-      elif 'endif' in v:
-        mergedTokenList[i] = v.replace('endif', 'ENDIF')
-      elif 'input' in v:
-        mergedTokenList[i] = v.replace('input', 'INPUT')
-      elif 'output' in v:
-        mergedTokenList[i] = v.replace('output', 'OUTPUT')
-      elif 'ref' in v:
-        mergedTokenList[i] = v.replace('ref', 'REF')
-      elif 'eof' in v:
-        mergedTokenList[i] = v.replace('eof', 'EOF')
-      elif 'error' in v:
-        mergedTokenList[i] = v.replace('error', 'ERROR')
-      elif  '=' in v:
-        nextChar = self.nextToken(filename)
-        if nextChar == '=':
-          # read char before return
-          mergedTokenList[i] = v.replace('==', 'EQUAL')
-        else:
-          mergedTokenList[i] = v.replace('=', 'ASSIGN')
-      elif '<' in v:
-        nextChar = self.nextToken(filename)
-        if nextChar == '=':
-          # read char before return
-          mergedTokenList[i] = v.replace('<=', 'LESSEQUAL')
-        else:
-          mergedTokenList[i] = v.replace('<', 'LESS')
-      # elif mergedTokenList[i].isalpha():
-      #   while(self.nextToken()):
-      #
-      # elif mergedTokenList[i].isnumeric():
-      #   while (self.nextToken()):
-
-      return tempMergedTokenList
+    #
+    # # return mergedTokenlist[index + 1]
+    # # tempMergedTokenList = mergedTokenList.copy()
+    # # return tempMergedTokenList.pop(0)
+    #
+    return mergedTokenList
 
 
   # nextToken should advance the scanner to the next token
-  def nextToken(self, filename):
-    list = self.tokenizer(filename)
-    return list.pop
+  def nextToken(self, filename, list):
+    list.pop(0)
+    return list #need to modify the actual list
+
 
   # currentToken should return the current token
-  def currentToken(self, filename):
-    return tempMergedTokenList[0]
-    # currentTok = self.nextToken(filename)
+  def currentToken(self, intialTokensList):
+    for i, v in enumerate(intialTokensList):
+      if ';' in v:
+        return Core.SEMICOLON
+      elif ',' in v:
+        return Core.COMMA
+      elif '!' in v:
+        return Core.NEGATION
+      elif '+' in v:
+        return Core.ADD
+      elif '-' in v:
+        return Core.SUB
+      elif '*' in v:
+        return Core.MULT
+      elif '(' in v:
+        return Core.LPAREN
+      elif ')' in v:
+        return Core.RPAREN
+      elif 'begin' in v:
+        return Core.BEGIN
+      elif 'program' in v:
+        return Core.PROGRAM
+      elif 'end' in v:
+        return Core.END
+      elif 'new' in v:
+        return Core.NEW
+      elif 'define' in v:
+        return Core.DEFINE
+      elif 'extends' in v:
+        return Core.EXTENDS
+      elif 'class' in v:
+        return Core.CLASS
+      elif 'endclass' in v:
+        return Core.ENDCLASS
+      elif 'int' in v:
+        return Core.INT
+      elif 'endfunc' in v:
+        return Core.ENDFUNC
+      elif 'if' in v:
+        return Core.IF
+      elif 'then' in v:
+        return Core.THEN
+      elif 'else' in v:
+        return Core.ELSE
+      elif 'while' in v:
+        return Core.WHILE
+      elif 'endwhile' in v:
+        return Core.ENDWHILE
+      elif 'endif' in v:
+        return Core.ENDIF
+      elif 'input' in v:
+        return Core.INPUT
+      elif 'output' in v:
+        return Core.OUTPUT
+      elif 'ref' in v:
+        return Core.REF
+      elif 'eof' in v:
+        return Core.EOF
+      elif 'error' in v:
+        return Core.ERROR
+      elif '==' in v:
+        return Core.EQUAL
+      elif '=' in v:
+        return Core.ASSIGN
+      elif '<' in v:
+        return Core.LESS
+      elif '<=' in v:
+        return Core.LESSEQUAL
+      elif v.isalpha():
+        return Core.ID
+      elif v.isnumeric():
+        return Core.CONST
+
+  # If the current token is ID, return the string value of the identifier
+	# Otherwise, return value does not matter
+  def getID(self, str):
+    if str.isalpha():
+      return str
+    else:
+      return "not alpha"
+
+  # If the current token is CONST, return the numerical value of the constant
+	# Otherwise, return value does not matter
+  def getCONST(self, numStr):
+    if numStr.isnumeric():
+      return numStr
+    else:
+      return "not digit"
+
 
     # if currentTok == ';':
     #   return Core.SEMICOLON
@@ -158,12 +166,74 @@ class Scanner:
     # #   while (self.nextToken()):
 
 
-  # If the current token is ID, return the string value of the identifier
-	# Otherwise, return value does not matter
-  def getID(self):
-    return ""
-
-  # If the current token is CONST, return the numerical value of the constant
-	# Otherwise, return value does not matter
-  def getCONST(self):
-    return 0
+    # for i, v in enumerate(mergedTokenList):
+    #   if ';' in v:
+    #     mergedTokenList[i] = v.replace(';', Core.SEMICOLON)
+    #   elif ',' in v:
+    #     mergedTokenList[i] = v.replace(',', Core.COMMA)
+    #   elif '!' in v:
+    #     mergedTokenList[i] = v.replace('!', Core.NEGATION)
+    #   elif '+' in v:
+    #     mergedTokenList[i] = v.replace('+', Core.ADD)
+    #   elif '-' in v:
+    #     mergedTokenList[i] = v.replace('-', Core.SUB)
+    #   elif '*' in v:
+    #     mergedTokenList[i] = v.replace('*', Core.MULT)
+    #   elif '(' in v:
+    #     mergedTokenList[i] = v.replace('(', Core.LPAREN)
+    #   elif ')' in v:
+    #     mergedTokenList[i] = v.replace(')', Core.RPAREN)
+    #   elif 'begin' in v:
+    #     mergedTokenList[i] = v.replace('begin', Core.BEGIN)
+    #   elif 'program' in v:
+    #     mergedTokenList[i] = Core.PROGRAM
+    #   elif 'end' in v:
+    #     mergedTokenList[i] = v.replace('end', Core.END)
+    #   elif 'new' in v:
+    #     mergedTokenList[i] = v.replace('new', Core.NEW)
+    #   elif 'define' in v:
+    #     mergedTokenList[i] = v.replace('define', Core.DEFINE)
+    #   elif 'extends' in v:
+    #     mergedTokenList[i] = v.replace('extends', Core.EXTENDS)
+    #   elif 'class' in v:
+    #     mergedTokenList[i] = v.replace('class', Core.CLASS)
+    #   elif 'endclass' in v:
+    #     mergedTokenList[i] = v.replace('endclass', Core.ENDCLASS)
+    #   elif 'int' in v:
+    #     mergedTokenList[i] = v.replace('int', Core.INT)
+    #   elif 'endfunc' in v:
+    #     mergedTokenList[i] = v.replace('endfunc', Core.ENDFUNC)
+    #   elif 'if' in v:
+    #     mergedTokenList[i] = v.replace('if', Core.IF)
+    #   elif 'then' in v:
+    #     mergedTokenList[i] = v.replace('then', Core.THEN)
+    #   elif 'else' in v:
+    #     mergedTokenList[i] = v.replace('else', Core.ELSE)
+    #   elif 'while' in v:
+    #     mergedTokenList[i] = v.replace('while', Core.WHILE)
+    #   elif 'endwhile' in v:
+    #     mergedTokenList[i] = v.replace('endwhile', Core.ENDWHILE)
+    #   elif 'endif' in v:
+    #     mergedTokenList[i] = v.replace('endif', Core.ENDIF)
+    #   elif 'input' in v:
+    #     mergedTokenList[i] = v.replace('input', Core.INPUT)
+    #   elif 'output' in v:
+    #     mergedTokenList[i] = v.replace('output', Core.OUTPUT)
+    #   elif 'ref' in v:
+    #     mergedTokenList[i] = v.replace('ref', Core.REF)
+    #   elif 'eof' in v:
+    #     mergedTokenList[i] = v.replace('eof', Core.EOF)
+    #   elif 'error' in v:
+    #     mergedTokenList[i] = v.replace('error', Core.ERROR)
+    #   elif '==' in v:
+    #     mergedTokenList[i] = v.replace('==', Core.EQUAL)
+    #   elif '=' in v:
+    #     mergedTokenList[i] = v.replace('=', Core.ASSIGN)
+    #   elif '<' in v:
+    #     mergedTokenList[i] = v.replace('<', Core.LESS)
+    #   elif '<=' in v:
+    #     mergedTokenList[i] = v.replace('<=', Core.LESSEQUAL)
+    #   elif v.isalpha():
+    #     mergedTokenList[i] = v.replace(v, Core.ID + "[" + v + "]")
+    #   elif v.isnumeric():
+    #     mergedTokenList[i] = v.replace(v, Core.CONST + "[" + v + "]")
