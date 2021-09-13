2d21f50c7dbed9535f7e8799acc98aa6870320a7
hm0416 <hm0416@Hifas-MBP-8.lan>
2021-09-12 20:02:52 -0400

    with proj 2 files

---
+++ Project1/Parser.py
@@ -0,0 +1,12 @@
+class Prog:
+    def ProgParse(self, tokensList):
+        return 0
+
+# class Decl-seq:
+#     def DeclSeqParse(self):
+#         return 0
+#
+# class Stmt-seq:
+#
+# class Decl:
+
