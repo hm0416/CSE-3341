20b524859307ca7173e6f9c4aca7f21953690357
hm0416 <hm0416@Hifas-MBP-8.lan>
2021-09-21 17:11:00 -0400

    sem class

--- Project1/Main.py
+++ Project1/Main.py
@@ -11,17 +11,19 @@ def main():
     symbolTableLocal = []
 
     root = Prog()
-
     root.parse(S)
-
     root.semantic(symbolTableGlobal, symbolTableLocal)
 
-    for ele in symbolTableLocal:
-        for ele2 in symbolTableGlobal:
-            if ele.isalpha() and len(ele) == 1:
-                if ele not in symbolTableGlobal:
-                    print("ERROR: local variable is not declared")
-                    quit()
+    # for ele in symbolTableLocal:
+    #     for ele2 in symbolTableGlobal:
+    #         ele = str(ele)
+    #         ele2 = str(ele2)
+    #         if ele.isnumeric():
+    #             continue
+    #         elif ele.isalpha() and len(ele) == 1:
+    #             if ele not in symbolTableGlobal:
+    #                 print("ERROR: local variable " + ele + " is not declared")
+    #                 quit()
 
 
     root.print(1)
